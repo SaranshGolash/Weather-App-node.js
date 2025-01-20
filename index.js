1acf88adb57b3696ea8df710e761ecbae8cebf4b
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2025-01-20 08:47:08 +0530

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import axios from "axios";
+const app = express();
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+
+// Serve the public folder as static files
+app.use(express.static("public"));
+
+// Render the index template with default values for weather and error
+app.get("/", (req, res) => {
+  res.render("index", { weather: null, error: null });
+});
+
+// Handle the /weather route
+app.get("/weather", async (req, res) => {
+  // Get the city from the query parameters
+  const city = req.query.city;
+  const apiKey = 'de3c7cd159cd642fc02e2b6cc19fe7bf'
+
+  // Add your logic here to fetch weather data from the API
+  const APIurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  // Render the index template with the weather data and error message
+  let weather;
+  let error = null;
+  try {
+    const response = await axios.get(APIurl);
+    weather = response.data;
+  } catch (error) {
+    weather = null;
+    error = "Error, Please try again!";
+  }
+  res.render("index", { weather, error });
+});
+
+// Start the server and listen on port 3000 or the value of the PORT environment variable
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
